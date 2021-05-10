72811cb6b8a2b9e5271bb19af1480a53bfdf7c09
Eli Pacheco <eli.pacheco55@gmail.com>
2021-05-10 11:26:19 -0600

    Cleaned up

--- eli_pacheco_resume.py
+++ eli_pacheco_resume.py
@@ -2,44 +2,53 @@
 # HELLO WORLD!  Welcome to my resume in python!  
 # I am open to any and all feedback: eli.pacheco55@gmail.com
 # This program MUST be run with python3 
+# Simply run this program in any IDE 
+# Once program is finished, it will write a .txt document for the reader as my resume.
 
-# header
+# header info
 name = 'Eli Pacheco' 
 location = 'Albuquerque, NM'
 pn = '(505) 321 - 5922'
 email = 'eli.pacheco55@outlook.com'
-header = f'\n\t\t{name}\n\t\t{location}\n\t\t{pn}\n\t\t{email}\n\n'
 
-# build education rows  
-school = 'University of New Mexico' 
-degree = 'BS, Computer Science'
-expected_grad_date = 'Expected grad date: 2023'
-GPA = 'Current GPA: 3.67\n'
-education = f'{school}: {degree}\n{expected_grad_date}\n{GPA}\n'
+header = f'\n\t\t{name}\n\t\t{location}\n\t\t{pn}\n\t\t{email}\n\n' # create the header string
+
+# education info
+education = f'EDUCATION:\n'
+schooldict = {'University of New Mexico': 'BA, Computer Science'} # key:value pair for UNM:degree
+egd = 'Expected grad date: 2023' 
+GPA = 'Current GPA: 3.67'
+for key,value in schooldict.items(): # use for loop to add key:value into education string 
+    education += f'{key}: {value}'
+education += f'\n{egd}\n{GPA}\n\n' # create the education string
 
 # professional experience 
-apple_rtl = 'PROFESSIONAL EXPERIENCE:\nApple - Operations, 2015-2021\n'
-action_rtl = '- Timely receiving and sending thousands of products within the confines of the store.\n- Adapting to an ever-changing market within hours or minutes to better serve our customers and clients.\n- Collaborating creative solutions, with a team, to placing a constantly expanding product line into dimensions that remain the same.\n\n'
-professional_experience = f'{apple_rtl}{action_rtl}'
+professional_experience = 'PROFESSIONAL EXPERIENCE:\nApple - Operations, 2015-2021\n'
+rtl_action1 = '- Timely receiving and sending thousands of products within the confines of the store.'
+rtl_action2 = '- Adapting to an ever-changing market within hours or minutes to better serve our customers and clients.'
+rtl_action3 = '- Collaborating creative solutions, with a team, to placing a constantly expanding product line into dimensions that remain the same.'
+professional_experience += f'{rtl_action1}\n{rtl_action2}\n{rtl_action3}\n' # create professional experience string 
+# nothing fancy here, open to suggestions.
 
 # internships and career experience 
-apple_fmr = 'INTERNSHIPS AND CAREER EXPERIENCE:\nApple - Firmware, Jan 2021- June 2021\n'
-action_fmr = '- Work closely with developers to write and adapt tools, in python, to help firmware teams read through logs more efficiently.\n- Systematically troubleshoot hardware, using proprietary applications, to delve into systems and correct behavior.\n- Used git and GitHub to create, edit, and push local branches to the remote repo.\n\n'
-internships = f'{apple_fmr}{action_fmr}'
+internships = 'INTERNSHIPS AND CAREER EXPERIENCE:\nApple - Firmware, Jan 2021- June 2021\n'
+intern_action1 = '- Work closely with developers to write and adapt tools, in python, to help firmware teams read through logs more efficiently.'
+intern_action2 = '- Systematically troubleshoot hardware, using proprietary applications, to delve into systems and correct behavior.'
+intern_action3 = '- Used git and GitHub to create, edit, and push local branches to the remote repo.'
+if intern_action1 not in internships: # using 'if' statement to add job responsibilities to string 
+    internships += f'{intern_action1}\n{intern_action2}\n{intern_action3}\n\n' # create internships string 
 
 # technical skills
-skill_string = "\nSkills:\n---------------------"
+skill_string = "\nSkills:\n---------------------\n" # initialize skills string, followed by skills as a list
 skill_list = ['python', 'Computer Science', 'Git', 'GitHub', 'Operating Systems', 'Mathematics', 'c/c++', 'MATLAB', 'SOLIDWORKS']
-skillset = ""
-for i in skill_list:
-    if i not in skillset:
-        skillset += f'{i}\n'
-skills_column = f'{skill_string}\n{skillset}'
+for i in skill_list: # use a 'for' loop to display skills a column 
+    if i not in skill_string:
+        skill_string += f'{i}\n'
 
 w = open('eli_pacheco_resume.txt', 'w')
 w.write(header)
 w.write(education)
 w.write(internships)
 w.write(professional_experience)
-w.write(skills_column)
+w.write(skill_string)
 w.close()
