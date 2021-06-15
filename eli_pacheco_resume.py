52b6d4a966100564b0c15e02ce78c2029a2253bf
Eli Pacheco <elipacheco@Elis-MacBook-Air.local>
2021-06-14 18:34:56 -0600

    fixed skills string

--- eli_pacheco_resume.py
+++ eli_pacheco_resume.py
@@ -24,15 +24,14 @@ def skills():
     global skill_string
     # creating a token for proficiency measurement
     token = ' * '
-    # technical skills
-    skill_string = f"Skills | Proficiency"
+    # initialize the skills string
+    skill_string += f"Skills | Proficiency"
     skill_string += '\n---------------------\n'
-    
     skill_list = [f'Unix Shell (bash & zsh) |{token*5}', f'git & GitHub |{token*5}', f'Python |{token*4}',  f'Computer Science |{token*4}',
                   f'Operating Systems |{token*4}',f'Advanced Mathematics |{token*4}', f'Creativity |{token*4}', f'Code Optimization |{token*3}', 
                   f'YAML files |{token*2}', f'Regex |{token*2}', f'C/C++ |{token*2}',f'MATLAB |{token*2}', f'SOLIDWORKS |{token*2}']
     for skill in skill_list: # use a 'for' loop to display skills in a column 
-        skill_string += f'\t{skill}\n'
+        skill_string += f'\t{skill}\n' # finalize the skills string
     return skill_list
 
 # classes of resume information for anyone to edit easily
